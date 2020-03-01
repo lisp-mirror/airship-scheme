3bc957c50976d04e34290f0cf3501845ca6d87c2
Michael Babich <michaelbabich7@gmail.com>
2020-02-29 21:46:14 -0500

    Collect the parsed tokens into a list

--- scheme-read.lisp
+++ scheme-read.lisp
@@ -2,7 +2,16 @@
 
 ;; TODO: Stub
 (defun scheme-read (stream)
-  (read-case (stream c)
-    ((:range #\0 #\9) (format t "~S~%" c) (cons :digit c))
-    ((:range #\A #\Z) (format t "~S~%" c) (cons :alphanumeric c))
-    ((:range #\a #\z) (format t "~S~%" c) (cons :alphanumeric c))))
+  ;; Tokenizer
+  ;;
+  ;; TODO: Support combining :repeat with :range
+  (loop :for match := (read-case (stream c)
+                        ;; Note: digit is technically a "subtype" of
+                        ;; alphanumeric but at the moment the
+                        ;; classification is simple conses.
+                        ((:range #\0 #\9) (format t "~S~%" c) (cons :digit c))
+                        ((:range #\A #\Z) (format t "~S~%" c) (cons :alphanumeric c))
+                        ((:range #\a #\z) (format t "~S~%" c) (cons :alphanumeric c))
+                        ((:or #\Space #\Newline #\Tab) (format t "~S~%" c) (cons :whitespace c)))
+        :while match
+        :collect match))
