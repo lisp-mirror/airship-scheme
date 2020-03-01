c57a1c11814fad5adcc5619fb1d4a8facaf5afa8
Michael Babich <michaelbabich7@gmail.com>
2020-02-29 20:52:19 -0500

    Add a very basic stub using the current version of READ-CASE

--- scheme-read.lisp
+++ scheme-read.lisp
@@ -2,4 +2,8 @@
 (in-package #:scheme)
 
 ;; TODO: Stub
-(defun scheme-read ())
+(defun scheme-read (stream)
+  (read-case (stream c)
+    ((:range #\0 #\9) (format t "~S~%" c) (cons :digit c))
+    ((:range #\A #\Z) (format t "~S~%" c) (cons :alphanumeric c))
+    ((:range #\a #\z) (format t "~S~%" c) (cons :alphanumeric c))))
