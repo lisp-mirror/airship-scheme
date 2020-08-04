f7bc8b8c199f8f32dcb924e4c3db60a8502b0b48
Michael Babich <michaelbabich7@gmail.com>
2020-08-03 23:11:47 -0400

    Simplify the scheme-read stub file
    The :or pattern is temporarily commented out until READ-CASE restores
    that functionality so the file compiles again.

--- scheme-read.lisp
+++ scheme-read.lisp
@@ -3,48 +3,13 @@
 (cl:in-package #:airship-scheme)
 
 ;; TODO: Stub
-(defun tokenize (stream)
+(defun scheme-read (stream)
   (loop :for match := (read-case (stream x)
                         ((:range #\0 #\9) (format t "~S~%" x) (cons :digit x))
                         ((:range #\A #\Z) (format t "~S~%" x) (cons :letter x))
                         ((:range #\a #\z) (format t "~S~%" x) (cons :letter x))
-                        ((:or #\Space #\Newline #\Tab) (format t "~S~%" x) (cons :whitespace x))
-                        ((:or #\( #\)) (format t "~S~%" x) (cons :parens x)))
+                        ;; ((:or #\Space #\Newline #\Tab) (format t "~S~%" x) (cons :whitespace x))
+                        ;; ((:or #\( #\)) (format t "~S~%" x) (cons :parens x))
+                        )
         :while match
         :collect match))
-
-(defun parse (tokens &optional literal?)
-  (loop :with tokens* := tokens
-        :for token := (car tokens*)
-        :with literal? := literal?
-        ;; TODO: balance parens
-        :until (or (endp tokens*) (eql token #\)))
-        :collect (if (eql token #\()
-                     (multiple-value-bind (result rest)
-                         (parse (cdr tokens*) literal?)
-                       (prog1 result
-                         (setf tokens* rest)))
-                     (prog1
-                         (etypecase token
-                           (character token)
-                           (string (let ((scheme-symbol (intern (invert-case token) 'scheme)))
-                                     (if literal?
-                                         scheme-symbol
-                                         (cons :variable scheme-symbol)))))
-                       (setf tokens* (cdr tokens*))))
-          :into parse-tree
-        :finally
-           ;; TODO: check for balanced parens
-           (if t
-               (return parse-tree)
-               (error "Unmatched parentheses"))))
-
-;;; TODO: stub
-(defun simplify (parse-tree)
-  parse-tree)
-
-;; TODO: Stub
-(defun scheme-read (stream)
-  (let* ((tokens (tokenize stream))
-         (parse-tree (parse tokens)))
-    (simplify parse-tree)))
